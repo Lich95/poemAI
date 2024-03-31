dc51e38b86d58e215b28ffe53741104b2cd822b2
lich-home <18701462006@163.com>
2024-03-31 20:29:07 +0800

    瀑布了！

--- src/components/waterfall/index.vue
+++ src/components/waterfall/index.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        <ul>
-                <li v-for="item in waterfallList">
-                    <h4>{{ item.title }}</h4>
-                    <div>{{ item.content }}</div>
-                    <el-button>{{ item.type }}</el-button>
-                </li>
-        </ul>
+        <div class="water wf1">
+            <div v-for="item in wfl[0]">
+                <h4>{{ item.title }}</h4>
+                <div>{{ item.content }}</div>
+                <el-button>{{ item.type }}</el-button>
+            </div>
+        </div>
+        <div class="water wf2">
+            <div v-for="item in wfl[1]">
+                <h4>{{ item.title }}</h4>
+                <div>{{ item.content }}</div>
+                <el-button>{{ item.type }}</el-button>
+            </div>
+        </div>
+        <div class="water wf3">
+            <div v-for="item in wfl[2]">
+                <h4>{{ item.title }}</h4>
+                <div>{{ item.content }}</div>
+                <el-button>{{ item.type }}</el-button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -25,32 +39,45 @@ export default {
         },
         waterfallList: {
             type: Array,
-            default: [{title:1,content:2,type:3},{title:1,content:'22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222',type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},{title:1,content:2,type:3},]
+            default: [{ title: 1, content: 2, type: 3 }, { title: 1, content: '2222啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发2', type: 3 }, { title: 1, content: '说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发', type: 3 }, { title: 1, content: 2, type: 3 }, { title: 1, content: 2, type: 3 }, { title: 1, content: 2, type: 3 }, { title: 1, content: 2, type: 3 }, { title: 1, content: 2, type: 3 }, { title: 1, content:'2222啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发啊谁说的发生发撒法沙发沙发2', type: 3 }, { title: 1, content: 2, type: 3 }, { title: 1, content: 2, type: 3 }, { title: 1, content: 2, type: 3 },]
         }
     },
     data() {
         return {
             waterfallCol: 3,
-            wfl:[],
+            wfl: [[], [], []],
         };
     },
     methods: {
-        init(){
-
+        init() {
+            this.wfl[0].push(this.waterfallList[0])
+            this.wfl[1].push(this.waterfallList[1])
+            this.wfl[2].push(this.waterfallList[2])
+            for (let i = 3; i < this.waterfallList.length; i++) {
+                setTimeout(() => {
+                    let heights = [document.getElementsByClassName('wf1')[0].offsetHeight, document.getElementsByClassName('wf2')[0].offsetHeight, document.getElementsByClassName('wf3')[0].offsetHeight]
+                    console.log(heights);
+                    let numIndex = heights.findIndex(x => x == Math.min(...heights));
+                    this.wfl[numIndex].push(this.waterfallList[i])
+                }, 0);
+            }
         },
     },
-    mounted() { 
-
+    mounted() {
         this.init()
-
     }
 };
 </script>
 
 
-<style scoped>
-ul li{
-    float: left;
-    width: 200px;
+<style scoped lang="scss">
+div.water {
+    display: inline-flex;
+    width: 300px;
+    flex-direction: column;
+
+    div {
+        word-break: break-all;
+    }
 }
 </style>
