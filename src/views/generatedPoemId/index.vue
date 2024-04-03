c6a0bc13fe2b85c6cf3274c28ea260453b5f4882
lich-home <18701462006@163.com>
2024-04-03 21:55:19 +0800

    布局ok

--- src/views/generatedPoemId/index.vue
+++ src/views/generatedPoemId/index.vue
@@ -1,18 +1,105 @@
 <template>
     <div>
-        <div>22222</div>
-        <div>22222</div>
-        <div>22222</div>
-        <div>22222</div>
-        <div>22222</div>
-        <div>22222</div>
+        <div style="margin:20px 0 30px">
+            <div class="respBox" v-show="true" ref="respBox">
+                <div class="title">
+                    {{ $t('poemai_generated_poem') }}
+                </div>
+                <div class="content" v-loading="respLoading">
+                    {{ ` In summer's heat, I find no solace
+                    A time for sun and fun, it's all a facade
+                    The warmth that's supposed to bring us joy
+                    Only brings me chills, and makes me annoyed
+
+                    The days are long, the nights are short
+                    I crave the coolness of the morning dew
+                    But even then, the sun rises high
+                    And steals away my peaceful mood anew
+
+                    The world is bright, but not so warm
+                    I yearn for shade, where I can rest
+                    From the harsh rays that burn like storms
+                    And make my heart feel oppressed
+
+                    Oh, how I wish for autumn's breeze
+                    To bring some relief from summer's sway
+                    When leaves will fall, and skies will gray
+                    And bring back balance to each day` }}
+                </div>
+                <div class="copyBtn">
+                    <el-button size="small" class="copyButton" @click="handleCopy" data-clipboard-target=".content">{{
+                        $t('poemai_copy_btn')
+                        }}<el-icon>
+                            <CopyDocument />
+                        </el-icon></el-button>
+                </div>
+            </div>
+
+        </div>
+        <div style="margin:0 auto;width:1200px;text-align: center">
+            <p>Poem for anniversary</p>
+            <waterfall class="wfDv"></waterfall>
+        </div>
     </div>
 </template>
 <script setup>
+import waterfall from "@/components/waterfall/index.vue";
 
 
 </script>
 <style scoped lang="scss">
+.respBox {
+    max-width: 760px;
+    background-color: #fff;
+    border-radius: 24px;
+    padding: 1em;
+    margin: 0 auto;
+    margin-top: 25px;
+    border: 1px solid #7730D0;
+    color: #1d2331;
 
-</style>   
- 
+    .title {
+        display: inline-flex;
+        font-weight: bold;
+
+        &::before {
+            content: '';
+            display: inline-block;
+            width: 12px;
+            height: 24px;
+            border-radius: 4px;
+            background: #7730D0;
+            margin-right: 5px;
+        }
+    }
+
+    .content {
+        text-align: center;
+        border-radius: 8px;
+        background: #7730D01A;
+        padding: 12px 20px;
+        margin-top: 12px;
+        white-space: pre-wrap;
+        font-size: 18px;
+    }
+
+    .copyBtn {
+        text-align: right;
+        margin-top: 12px;
+        ;
+
+        .el-button {
+            background: #7730D0;
+            color: #fff;
+            border-radius: 16px;
+            padding: 7px 16px 7px 16px;
+            font-size: 16px;
+            height: 32px;
+
+            .el-icon {
+                margin-left: 5px;
+            }
+        }
+    }
+}
+</style>
--- src/views/generatedPoemType/index.vue
+++ src/views/generatedPoemType/index.vue
@@ -1,13 +1,30 @@
 <template>
     <div>
-    222222222222225
+        <p class="typeTitle">
+            {{ route.params.GeneratedPoemType }}
+        </p>
+
+        <div style="margin:0 auto;width:1200px;text-align: center">
+
+            <waterfall class="wfDv"></waterfall>
+        </div>
     </div>
 </template>
 <script setup>
+import { useRoute, useRouter } from 'vue-router';
+import waterfall from "@/components/waterfall/index.vue";
+const router = useRouter()
+const route = useRoute();
 
 
 </script>
 <style scoped lang="scss">
-
-</style>   
- 
+.typeTitle {
+    text-align: center;
+    color: #1D2331;
+    margin: 35px 0 30px;
+    font-size: 22px;
+    font-weight: 700;
+    line-height: 30px;
+}
+</style>
