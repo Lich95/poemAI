1287aa9aa7ee6a12c50501786da64cbebe37fb57
lc <18701462006@163.com>
2024-06-28 16:18:39 +0800

    stable

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import headerStyle from '@/views/header/index.vue'
-import AdSense from './components/AdSense.vue'
+import headerStyle from "@/views/header/index.vue";
+import AdSense from "./components/AdSense.vue";
 </script>
 
 <template>
   <div>
     <headerStyle></headerStyle>
     <router-view></router-view>
-    <AdSense class="ad-left" ad-slot="9524179685"/>
-    <AdSense class="ad-right" ad-slot="9524179685"/>
-    <AdSense class="ad-bottom" ad-slot="9520839519"/>
+    <AdSense class="ad-left" ad-slot="9524179685" />
+    <AdSense class="ad-right" ad-slot="9524179685" />
+    <AdSense class="ad-bottom" ad-slot="9520839519" />
+    <AdSense class="ad-bottom-mobile" ad-slot="7827954630" />
   </div>
 </template>
 
@@ -21,22 +22,40 @@ import AdSense from './components/AdSense.vue'
 .arRightType {
   float: right;
   text-align: right;
-    padding-right: 20px;
+  padding-right: 20px;
   &::before {
     right: 0;
   }
 }
 
-.ad-left, .ad-right {
+.ad-left,
+.ad-right {
   width: 280px;
   height: 600px; /* 你可以根据需要调整宽度 */
   position: fixed;
   top: 80px; /* 使广告在页面滚动时保持在视口中 */
 }
 .ad-left {
-  left:10px;
+  left: 10px;
 }
 .ad-right {
-  right:10px;
+  right: 10px;
+}
+.ad-bottom-mobile{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: none;
+}
+
+@media only screen and (max-width: 767px) {
+  .ad-left,
+  .ad-right,
+  .ad-bottom {
+    display: none;
+  }
+  .ad-bottom-mobile{
+    // display: block;
+  }
 }
 </style>
