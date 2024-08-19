cd913170e5a560b590a9bdaa8166472caa9e839d
lc <18701462006@163.com>
2024-08-19 16:31:36 +0800

    gongsi

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
-<script setup>
-import headerStyle from "@/views/header/index.vue";
+<script setup>import headerStyle from "@/views/header/index.vue";
 import AdSense from "./components/AdSense.vue";
-import { onMounted, onBeforeUnmount } from 'vue';
-onMounted(() => {
-  //   console.log('bangidng');
-  //   document.addEventListener('visibilitychange', handleVisibilityChange);
+import { onMounted, onBeforeUnmount ,ref} from 'vue';
+
+
+const googletag = ref(null);
+onMounted(async () => {
+  try {
+    await loadGPTScript();
+    if (googletag.value && googletag.value.queryIds) {
+      googletag.value.queryIds.push(() => {
+        googletag.value.pubads().enableSingleRequest();
+        googletag.value.enableServices();
+      });
+    } else {
+      console.error('googletag or queryIds not properly initialized');
+    }
+  } catch (error) {
+    console.error('Error loading GPT script:', error);
+  }
+
   handleVisibilityChange()
 });
 
-// onBeforeUnmount(() => {
-//   console.log('jiebang');
-//   document.removeEventListener('visibilitychange', handleVisibilityChange);
-// });
-const handleVisibilityChange = () => {
-  setInterval(() => {
-    if (document.visibilityState === 'visible') {
-      console.log('refresh');
-      googletag.pubads().refresh()
+const loadGPTScript = () => {
+  return new Promise((resolve, reject) => {
+    if (!window.googletag) {
+      window.googletag = { queryIds: [] };
+      const script = document.createElement("script");
+      script.src = "https://securepubads.g.doubleclick.net/tag/js/gpt.js";
+      script.async = true;
+      script.onload = () => {
+        googletag.value = window.googletag;
+        resolve();
+      };
+      script.onerror = (error) => reject(error);
+      document.head.appendChild(script);
+    } else {
+      googletag.value = window.googletag;
+      resolve();
     }
-  }, 30000);
+  });
 };
+const refreshAds = () => {
+  if (googletag.value && googletag.value.queryIds) {
+    googletag.value.queryIds.push(() => {
+      googletag.value.pubads().refresh();
+    });
+  } else {
+    console.error('googletag or queryIds not properly initialized');
+  }
+};
+const handleVisibilityChange = () => {
+  const intervalId = setInterval(() => {
+    if (document.visibilityState === 'visible' && googletag.value) {
+      refreshAds()
+    }
+  }, 60000);
 
+  onBeforeUnmount(() => {
+    clearInterval(intervalId);
+  });
+};
 </script>
 
 <template>
