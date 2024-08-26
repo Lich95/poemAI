1b5139bba574c37436200a955771564715ff7e66
lc <18701462006@163.com>
2024-08-26 22:01:35 +0800

    全页面删掉

--- src/App.vue
+++ src/App.vue
@@ -67,10 +67,6 @@ const handleVisibilityChange = () => {
   <div>
     <headerStyle></headerStyle>
     <router-view></router-view>
-    <AdSense class="ad-left" ad-slot="9524179685" />
-    <AdSense class="ad-right" ad-slot="9524179685" />
-    <!-- <AdSense class="ad-bottom" ad-slot="9520839519" /> -->
-    <!-- <AdSense class="ad-bottom-mobile" ad-slot="7827954630" /> -->
   </div>
 </template>
 
@@ -89,37 +85,4 @@ const handleVisibilityChange = () => {
   }
 }
 
-.ad-left,
-.ad-right {
-  /* 你可以根据需要调整宽度 */
-  position: fixed;
-  top: 80px;
-  /* 使广告在页面滚动时保持在视口中 */
-}
-
-.ad-left {
-  left: 10px;
-}
-
-.ad-right {
-  right: 10px;
-}
-// .ad-bottom-mobile{
-//   position: fixed;
-//   bottom: 0;
-//   width: 100%;
-//   display: none;
-// }
-
-@media only screen and (max-width: 767px) {
-
-  .ad-left,
-  .ad-right,
-  .ad-bottom {
-    display: none;
-  }
-  // .ad-bottom-mobile{
-  //   // display: block;
-  // }
-}
 </style>
